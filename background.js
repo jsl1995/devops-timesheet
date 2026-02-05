4a8afa526320e13bb0681d74326fcb06246899d0
Joel <joel@Mac.localdomain>
2026-02-05 09:19:17 +0000

    Revert "Auto-detect Azure DevOps theme and sync to extension"
    This reverts commit f6b1b73fe0f4c519d955a0f5bd53d9fc2c995825.

--- background.js
+++ background.js
@@ -1,9 +1,1 @@
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
-
-// Relay theme detection from content script to side panel
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'THEME_DETECTED') {
-    // Store the detected theme
-    chrome.storage.local.set({ detectedTheme: message.theme });
-  }
-});
